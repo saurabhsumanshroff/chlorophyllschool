1a0eedbc1e8f7c5ed1e3463596826f318dde47d0
Suman <vx88@INMAC62FGF.local>
2018-01-05 15:26:52 +0530

    footer sdtyling

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {Carousel} from 'react-bootstrap';
 import '../Stylesheets/Footer.css';
-import carousel from '../Resources/carousel.jpg';
 
 class Footer extends Component {
   render() {
@@ -9,18 +8,26 @@ class Footer extends Component {
       <div className="footer">
         <Carousel id="footerCarousel" >
           <Carousel.Item>
-            <p>Any Text-1 you want to display</p>
+            <p>Any Text-1 you want to displayAny Text-1 you want to displayAny Text-1 you want to displayAny Text-1 you want to display</p>
           </Carousel.Item>
           <Carousel.Item>
-            <p>Any Text-2 you want to display.</p>
+            <p>Any Text-1 you want to displayAny Text-2 you want to displayAny Text-1 you want to displayAny Text-1 you want to display</p>
           </Carousel.Item>
           <Carousel.Item>
-            <p>Any Text-3 you want to display</p>
+            <p>Any Text-1 you want to displayAny Text-3 you want to displayAny Text-1 you want to displayAny Text-1 you want to display</p>
           </Carousel.Item>
         </Carousel>
         <div className="copyright">
           © copyright 2017 all rights reserved by, Chlorophyll School, Jhumri Telaiya
         </div>
+        <div className="col-sm-4 download-app">
+          <h5>DOWNLOAD MOBILE APPLICATION OF ST FRANCIS SCHOOL DEOGHAR</h5>
+          <div className="app-box">
+          	<a href="javascript:void(0)"><img alt="" src="images/google-play.png" /></a>
+            <a href="javascript:void(0)"><img alt="" src="images/apple.png" /></a>
+            <a href="javascript:void(0)"><img alt="" src="images/windows.png" /></a>
+          </div>
+          </div>
       </div>
     );
   }
