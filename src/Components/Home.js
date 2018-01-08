3b1997de581e9f3b54aeacf15b4d00c692c66a81
Saurabh <saurabhsumanshroff@gmail.com>
2018-01-08 17:04:53 +0530

    Event calander, Login and school news

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import BannerCarousel from './BannerCarousel.js';
+import Marquee from './Marquee.js';
+import '../Stylesheets/Home.css';
+import CalendarIcon from '../Resources/calendar-icon.png';
+import LoginIcon from '../Resources/portal-login-icon.png';
 
 class Home extends Component {
   render() {
     return (
       <div className="home">
         <BannerCarousel />
+        <div className="row loginCalendarNews">
+          <div className="col-sm-3 calenderIcon">
+            <img alt="Login" src={CalendarIcon} />
+            <p> Calender Events </p>
+          </div>
+          <div className="col-sm-3 loginIcon">
+            <img alt="Login" src={LoginIcon} />
+            <p> Login </p>
+          </div>
+          <div className="col-sm-6 newsTile">
+            <div className="newsVerticalScroll">
+              <h2>School News</h2>
+              <Marquee />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/Components/Marquee.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../Stylesheets/Marquee.css';
+
+class Marquee extends Component {
+  render() {
+    return (
+      <div className="marquee">
+        <p> Text 1Text 1Text 1Text 1Text 1Text 1Text 1Text 1Text 1Text 1Text 1Text 1Text 1Text 1Text 1Text 1Text 1Text 1Text 1Text 1Text 1Text 1Text 1 </p>
+      </div>
+    );
+  }
+}
+
+export default Marquee;
