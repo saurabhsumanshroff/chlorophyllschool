22846260c83dba8afb8b741f0998ddfa0f8423c8
Saurabh <saurabhsumanshroff@gmail.com>
2018-01-08 12:41:47 +0530

    Footer fixed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,28 +6,32 @@ class Footer extends Component {
   render() {
     return (
       <div className="footer">
-        <Carousel id="footerCarousel" >
-          <Carousel.Item>
-            <p>Any Text-1 you want to displayAny Text-1 you want to displayAny Text-1 you want to displayAny Text-1 you want to display</p>
-          </Carousel.Item>
-          <Carousel.Item>
-            <p>Any Text-1 you want to displayAny Text-2 you want to displayAny Text-1 you want to displayAny Text-1 you want to display</p>
-          </Carousel.Item>
-          <Carousel.Item>
-            <p>Any Text-1 you want to displayAny Text-3 you want to displayAny Text-1 you want to displayAny Text-1 you want to display</p>
-          </Carousel.Item>
-        </Carousel>
+        <div className="row">
+          <div className="col-sm-8">
+            <Carousel id="footerCarousel" >
+              <Carousel.Item>
+                <p>Any Text-1 you want to displayAny Text-1 you want to displayAny Text-1 you want to displayAny Text-1 you want to display</p>
+              </Carousel.Item>
+              <Carousel.Item>
+                <p>Any Text-1 you want to displayAny Text-2 you want to displayAny Text-1 you want to displayAny Text-1 you want to display</p>
+              </Carousel.Item>
+              <Carousel.Item>
+                <p>Any Text-1 you want to displayAny Text-3 you want to displayAny Text-1 you want to displayAny Text-1 you want to display</p>
+              </Carousel.Item>
+            </Carousel>
+          </div>
+          <div className="col-sm-4 download-app">
+            <h5>Copyright wala image</h5>
+            <div className="app-box">
+              <a href="javascript:void(0)"><img alt="" src="images/google-play.png" /></a>
+              <a href="javascript:void(0)"><img alt="" src="images/apple.png" /></a>
+              <a href="javascript:void(0)"><img alt="" src="images/windows.png" /></a>
+            </div>
+          </div>
+        </div>
         <div className="copyright">
           © copyright 2017 all rights reserved by, Chlorophyll School, Jhumri Telaiya
         </div>
-        <div className="col-sm-4 download-app">
-          <h5>DOWNLOAD MOBILE APPLICATION OF ST FRANCIS SCHOOL DEOGHAR</h5>
-          <div className="app-box">
-          	<a href="javascript:void(0)"><img alt="" src="images/google-play.png" /></a>
-            <a href="javascript:void(0)"><img alt="" src="images/apple.png" /></a>
-            <a href="javascript:void(0)"><img alt="" src="images/windows.png" /></a>
-          </div>
-          </div>
       </div>
     );
   }
